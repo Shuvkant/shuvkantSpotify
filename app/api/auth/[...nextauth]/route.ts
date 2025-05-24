bb8ad78f9d823c8cc7bb326e0cb6178fa08dafea
Shuvkant <weird426344@gmail.com>
2025-05-24 22:33:49 +0545

    added user count

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,32 @@
-import GoogleProvider from "next-auth/providers/google";
-import NextAuth from "next-auth"
+import GoogleProvider from 'next-auth/providers/google'
+import NextAuth from 'next-auth'
+import { signIn, useSession, signOut } from 'next-auth/react'
+import { prismaClient } from '@/app/lib/db'
 
-const handler=NextAuth({
-  providers:[
-  GoogleProvider({
-    clientId: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET
-  })
-  ]
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async signIn(params) {
+      if (!params.user.email) {
+        return false
+      }
+      console.log(params)
+      try {
+        await prismaClient.user.create({
+          data: {
+            email: params.user.email,
+            provider: 'Google'
+          },
+        })
+      } catch (error) {}
+      return true
+    },
+  },
 })
- 
-export {handler as GET, handler as POST}
+
+export { handler as GET, handler as POST }
---
+++ app/api/streams/route.ts
@@ -0,0 +1,54 @@
+import {NextRequest, NextResponse} from "next/server"
+import {z} from "zod";
+var YT_REGEX =/^(?:https?:\/\/)?(?:www\.)?(?:m\.)?(?:youtube\.com\/(?:watch\?(?!.*\blist=)(?:.*&)?v=|embed\/|v\/)|youtu\.be\/)([a-zA-Z0-9_-]{11})(?:[?&]\S+)?$/;
+const CreateStreamSchema =z.object({
+  creatorId:z.string(),
+  url:z.string()
+})
+
+export async function POST(req:NextRequest){
+  try {
+    const data= CreateStreamSchema.parse(await req.json());
+    const isYt=data.url.match(YT_REGEX)
+    if(!isYt){
+      return NextResponse.json({
+        message:"Wrong URL format"
+      },
+        {
+          status:411
+        }
+      )
+    }
+    const extractedId=data.url.split("?v=")[1];
+   const stream=await prismaClient.stream.create({
+      data:{
+        userId:data.creatorId,
+        url:data.url,
+        extractedId,
+        type:"Youtube"
+      }
+    })
+    return NextResponse.json({
+      message:"Added Stream",
+      id:stream.id
+    })
+  } catch (error) {
+    return NextResponse.json({
+      message:"Error while adding a stream"
+    }, {
+        status:411
+      })
+  }
+}
+
+export async function GET(req:NextRequest){
+  const creatorId=req.nextUrl.searchParams.get("creatorId");
+  const streams=await prismaClient.stream.findMany({
+    where:{
+      userId:creatorId ?? ""
+    }
+  })
+  return NextResponse.json({
+    streams
+  })
+}
