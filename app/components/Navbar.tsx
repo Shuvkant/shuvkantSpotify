2541a742317e75827e8cdb972e10b9ff7c6d47e2
Shuvkant <weird426344@gmail.com>
2025-06-02 23:56:49 +0545

    adding button for dashboard

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,7 @@
+import { Button } from '@/components/ui/button'
+;<Button
+  className='bg-gradient-to-r from-fuchsia-600 to-indigo-600 hover:from-fuchsia-700 hover:to-indigo-700 text-white border-0 shadow-lg shadow-fuchsia-500/25'
+  onClick={}
+>
+  Dashboard
+</Button>
